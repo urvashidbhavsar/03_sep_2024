30f8d4d41722126f7152baac5fbddbb51cfd4c6d
urvashi <urvashi.d.bhavsar@gmail.com>
2025-04-09 11:22:30 +0530

    9th changes

--- React/jsonexample/src/pages/Showemp.jsx
+++ React/jsonexample/src/pages/Showemp.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons'
+import List from './List'
+import Updateemp from './Updateemp'
 
 const Showemp = () => {
     const [getdata, setGetdata] = useState([])
+    const [update, setUpdate] = useState("")
+
+    const editData = (items) => {
+        // setEdit(items)
+        setUpdate(items)
+    }
 
     const fetchapidata = async () => {
         await fetch("http://localhost:3000/employee", {
@@ -21,6 +31,15 @@ const Showemp = () => {
         fetchapidata()
     }, [])
 
+    const DeleteData = async (id) => {
+        if (confirm("Are you sure to want delete this data?")) {
+            await fetch(`http://localhost:3000/employee/${id}`, {
+                method: "DELETE",
+            })
+        }
+        fetchapidata()
+    }
+
     return (
         <>
             <div className="container">
@@ -33,6 +52,7 @@ const Showemp = () => {
                             <th>Designation</th>
                             <th>Salary</th>
                             <th>Profile</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -46,11 +66,21 @@ const Showemp = () => {
                                     <td>
                                         <img src={item.profile} style={img} />
                                     </td>
+                                    <td>
+                                        <button className='btn btn-info mx-1' data-bs-toggle="modal" data-bs-target="#updatemodal" onClick={() => editData(item)}>
+                                            <FontAwesomeIcon icon={faPen} />
+                                        </button>
+                                        <button className="btn btn-danger mx-1" onClick={() => DeleteData(item.id)}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         }
                     </tbody>
                 </table>
+
+                <Updateemp edit={update} setEdit={setUpdate} fetchdata={fetchapidata} />
             </div>
         </>
     )
--- React/jsonexample/src/pages/Updateemp.jsx
+++ React/jsonexample/src/pages/Updateemp.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import List from './List'
 
-const Updateemp = () => {
+const Updateemp = ({ edit, setEdit, fetchdata }) => {
+    // edit for update state
+    // setedit for setupdate which is func to change state
+    // fetchdata for fetchapidata which is reload data
+    const des = List()
+    const handleChange = (e) => {
+        setEdit({ ...edit, [e.target.name]: e.target.value })
+    }
+
+    const updateData = async (e) => {
+        e.preventDefault()
+        await fetch(`http://localhost:3000/employee/${edit.id}`, {
+            method: "PUT",
+            body: JSON.stringify(edit)
+        })
+        alert("data updated!!!")
+        fetchdata()
+    }
     return (
         <>
-
+            <div className="modal fade" id="updatemodal" tabIndex="-1" aria-labelledby="updatemodalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="updatemodalLabel">Update Data</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="row g-2">
+                                    <div className="col-12">
+                                        <h5>ID: {edit.id}</h5>
+                                    </div>
+                                    <div className="col-12">
+                                        <input type="text" name="empname" placeholder='Full name' className='form-control' onChange={handleChange} value={edit.empname} />
+                                    </div>
+                                    <div className="col-12">
+                                        <input type="number" name="salary" placeholder='Salary' className='form-control' onChange={handleChange} value={edit.salary} />
+                                    </div>
+                                    <div className="col-12">
+                                        <select name="designation" className='form-select' onChange={handleChange} value={edit.designation}>
+                                            <option value="">--- Select Designation ---</option>
+                                            {
+                                                des.map(des =>
+                                                    <option key={des} value={des}>
+                                                        {des}
+                                                    </option>
+                                                )
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="col-12">
+                                        <input type="text" name="profile" className='form-control' placeholder='Image URL' onChange={handleChange} value={edit.profile} />
+                                    </div>
+                                    <div className="col-12">
+                                        <button className='btn btn-primary' onClick={updateData}>Update</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
