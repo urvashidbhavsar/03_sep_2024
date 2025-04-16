7e731bf5628420d343d67f4ae29a41580c88f515
urvashi <urvashi.d.bhavsar@gmail.com>
2025-04-16 11:20:33 +0530

    16th changes

--- React/firebase_example/src/LoginAuth/Home.jsx
+++ React/firebase_example/src/LoginAuth/Home.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { signOut } from 'firebase/auth'
+import { auth, db } from '../firebase'
+import { useNavigate } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
 
 const Home = () => {
-    return (
-        <div>
+    const navigate = useNavigate()
+    const [getuser, setGetuser] = useState("")
 
-        </div>
+    const fetchuser = async () => {
+        const user = auth.currentUser;
+        if (user) {
+            let userexist = await getDoc(doc(db, "regusers", user.uid))
+            setGetuser(userexist.data())
+        }
+    }
+
+    useEffect(() => {
+        fetchuser();
+    })
+
+    const logout = async () => {
+        await auth.signOut()
+        navigate("/")
+    }
+
+    return (
+        <>
+            <div className='w-50 m-auto shadow p-5 mt-5 text-center'>
+                {
+                    getuser ?
+                        <h1>Welcome, {getuser.fullname}!!!!</h1>
+                        :
+                        <div className="spinner-border text-primary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                }
+
+                <div className='my-2'>
+                    <button onClick={logout} className='btn btn-danger'>Logout</button>
+                </div>
+            </div>
+        </>
     )
 }
 
